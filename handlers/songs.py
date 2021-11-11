759b186b61c8dd197cae77a0bd7a294b44796f79
Harsh-722 <85775284+Harsh-722@users.noreply.github.com>
2021-11-11 09:43:03 +0530

    Update songs.py

--- handlers/songs.py
+++ handlers/songs.py
@@ -8,8 +8,8 @@ import sys
 import time
 from youtubesearchpython import SearchVideos
 from pyrogram import filters, Client
-from youtube_dl import YoutubeDL
-from youtube_dl.utils import (
+from yt_dlp import YoutubeDL
+from yt_dlp.utils import (
     ContentTooShortError,
     DownloadError,
     ExtractorError,
@@ -22,7 +22,7 @@ from youtube_dl.utils import (
 
 @Client.on_message(filters.command("song") & ~filters.edited)
 async def song(client, message):
-    cap = "@JEBotZ"
+    cap = "@Harsh7_Musicbot"
     url = message.text.split(None, 1)[1]
     rkp = await message.reply("Processing...")
     if not url:
